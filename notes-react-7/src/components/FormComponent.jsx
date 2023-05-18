d78510ff3f535d77709879256c65599625fd80d9
Mateus Rosengarten Mauricio <108750288+Mateus1rosengarten@users.noreply.github.com>
2023-05-18 14:29:23 +0300

    first commit

---
+++ notes-react-7/src/components/FormComponent.jsx
@@ -0,0 +1,14 @@
+
+
+function FormComponent ({onTitle,onText,onNote}) {
+    return(
+        <div className='notebox'>
+        <input className="inputStyle"  onChange={onTitle} placeholder='title'></input>
+        <textarea className="textStyle" onChange={onText} placeholder='your note...'></textarea>
+        <button className='addbutton' onClick={onNote}>Add</button>
+      </div>
+    )
+}
+
+
+export default FormComponent
